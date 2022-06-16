472d6da9cca0241357ab2530a4a0b7550b87e764
Steffan Perera <steffanperera1997@gmail.com>
2022-06-16 23:00:56 +0530

    test

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 function App() {
-    return <div>Hello World!</div>;
+    return (
+        <div>
+            <Header />
+            <AddContact />
+            <ContactList />
+        </div>
+    );
 }
 
 export default App;
