2f604f00cdd73b3479fcb40dc318901ee405d0a4
Steffan Perera <steffanperera1997@gmail.com>
2022-06-22 21:26:59 +0530

    commit

---
+++ react-context-hooks/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <h1>Context App</h1>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
